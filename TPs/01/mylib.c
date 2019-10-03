8c2ec09080419a136cd946933a0bc2696339bf75
Frozander <ceylan.mustafabatuhan@gmail.com>
2019-10-03 11:21:17 +0300

    Added TP01

---
+++ TPs/01/mylib.c
@@ -0,0 +1,8 @@
+#include "mylib.h"
+
+
+int isLetter(char c){
+    // a remplir
+}
+
+// ...
---
+++ TPs/01/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include "mylib.h"
+
+// to complile from terminal, run: gcc test.c mylib.c -o ApplicationName -lm
+// to run the complied application, run: ./ApplicatioName
+
+int main(void)
+{
+    // a remplir
+    
+    // Test 1
+    //....
+    
+    // test 2
+    // ...
+    
+    return 0;
+}
