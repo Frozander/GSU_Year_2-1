32e7c8aa78bb89f91055be3d2d92e0c24025d251
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2019-10-18 03:23:31 +0300

    [TP03] Added menu

--- TPs/03/main.c
+++ TPs/03/main.c
@@ -1,10 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #include "tree.h"
+#include "menu.h"
 
-int main(int argc, char const *argv[])
+int main()
 {
+    /*
+      _______ ______  _____ _______ _____ 
+     |__   __|  ____|/ ____|__   __/ ____|
+        | |  | |__  | (___    | | | (___  
+        | |  |  __|  \___ \   | |  \___ \ 
+        | |  | |____ ____) |  | |  ____) |
+        |_|  |______|_____/   |_| |_____/ 
+    
     Tree* my_tree;
     
     my_tree = add_node(my_tree, "Turkey", 90);
@@ -33,6 +43,109 @@ int main(int argc, char const *argv[])
     printf("\n");
     printf("\n");
 
-    print_tree(my_tree, P_LEVELORDER);    
+    print_tree(my_tree, P_LEVELORDER);
+    */
+
+    int menu_state = 0;
+
+    Tree* main_tree;
+
+    Tree* temp;
+
+    char input_str[30];
+    int input_code;
+
+    while (menu_state != M_EXIT)
+    {
+        switch (menu_state)
+        {
+        case M_MAIN:
+
+            printf("1-Add Country\n");
+            printf("2-Remove Country\n");
+            printf("3-Find Max and Min Country\n");
+            printf("4-Print Contact\n");
+            printf("5-EXIT\n");
+
+            scanf("%d", &menu_state);
+            break;
+        
+        case M_ADD:
+
+            printf("Country Name: ");
+            scanf("%s", input_str);
+            printf("Country Code: ");
+            scanf("%d", &input_code);
+
+            main_tree = add_node(main_tree, input_str, input_code);          
+
+            menu_state = M_MAIN;
+            break;
+
+        case M_DEL:
+
+            printf("Code of the country to delete: ");
+            scanf("%d", &input_code);
+            main_tree = remove_node(main_tree, input_code);
+            
+            menu_state = M_MAIN;
+            break;
+
+        case M_SRCH:
+            
+            temp = findMin(main_tree);
+            printf("Min: %d - %s\n", temp->code, temp->country);
+            temp = findMax(main_tree);
+            printf("Max: %d - %s\n", temp->code, temp->country);
+
+            menu_state = M_MAIN;
+            break;
+
+        case M_PRNT:
+
+            printf("Select Notation: ");
+            scanf("%s", input_str);
+            int i = 0;
+            // SANITIZE
+            while (input_str[i] != '\0')
+            {
+                input_str[i] = tolower(input_str[i]);
+                i++;
+            }
+            
+            if (strcmp(input_str, "postorder") == 0)
+            {
+                print_tree(main_tree, P_POSTORDER);
+            } else if (strcmp(input_str, "inorder") == 0)
+            {
+                print_tree(main_tree, P_INORDER);
+            } else if (strcmp(input_str, "preorder") == 0)
+            {
+                print_tree(main_tree, P_PREORDER);
+            } else if (strcmp(input_str, "levelorder") == 0)
+            {
+                print_tree(main_tree, P_LEVELORDER);
+            } else
+            {
+                printf("Possible notations: postorder - inorder - preorder - levelorder");
+            }
+
+            menu_state = M_MAIN;
+            break;
+
+        case M_EXIT:
+            printf("Exiting!\n");
+            break;
+        
+        default:
+            printf("Unknown Input, reseting menu!\n");
+
+            menu_state = M_MAIN;
+            break;
+        }
+
+        
+    }
+    
     return 0;
 }
---
+++ TPs/03/menu.h
@@ -0,0 +1,12 @@
+#ifndef __MENU_H_
+#define __MENU_H_
+
+// Menu defines
+#define M_MAIN  0
+#define M_ADD   1
+#define M_DEL   2
+#define M_SRCH  3
+#define M_PRNT  4
+#define M_EXIT  5
+
+#endif
