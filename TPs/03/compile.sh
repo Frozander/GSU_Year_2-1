90727141d3098e3e7d50fc95cf2624cb447b7d65
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2019-10-20 12:59:12 +0300

    [TP03] Commented the tests from compile script

--- TPs/03/compile.sh
+++ TPs/03/compile.sh
@@ -1,3 +1,7 @@
-gcc bst.c tree.c -o BST_test
-gcc avl.c tree.c -o AVL_test
+# Aynı programlar ama 7.soruda istenen eklemeler yok
+# ve farklı yöntemler kullanıyorlar (BST ve AVL)
+
+#gcc bst.c tree.c -o BST_test
+#gcc avl.c tree.c -o AVL_test
+
 gcc main.c tree.c -o MAIN_app
