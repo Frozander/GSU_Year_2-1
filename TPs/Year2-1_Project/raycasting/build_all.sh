8c8038ef0f24a13b6558f4887287fe413ec6fe68
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2019-12-16 11:03:00 +0300

    [Project] Added raytracing code and texture assets

---
+++ TPs/Year2-1_Project/raycasting/build_all.sh
@@ -0,0 +1,2 @@
+g++ raycasting_textured.cpp ./libs/quickcg/quickcg.cpp -lSDL -O3 -W -Wall -ansi -pedantic -o wolfenstein
+g++ raycasting_flat.cpp ./libs/quickcg/quickcg.cpp -lSDL -O3 -W -Wall -ansi -pedantic -o flat_walls
