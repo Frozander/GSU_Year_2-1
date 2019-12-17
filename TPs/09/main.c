2e5205ec6f11df38e2753dd9063679cadda406f5
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2019-12-17 21:18:42 +0300

    [TP09] Added main.c Main includes a few definition checks using the C Preprocessor definitions of compilers. Source code won't compile if MSVC or Intel compiler is used because one of the library funtions use ranges in switch-case.

---
+++ TPs/09/main.c
@@ -0,0 +1,22 @@
+// Checks if case range used in note_to_grade function is supported by the compiler
+#if (defined(__GNUC__) ||defined(__GNUG__) || defined(__clang__))
+    #define HAS_GNU_CLANG
+#endif
+
+#if defined(_MSC_VER) && defined(__INTEL_COMPILER)
+    #define HAS_MSVC_OR_INTEL
+#endif
+
+#if (defined(HAS_GNU_CLANG) && !defined(HAS_MSVC_OR_INTEL))
+    #define CASE_RANGE_SUPPORT
+#endif
+// Error if not
+#ifndef CASE_RANGE_SUPPORT
+    #error "Your compiler does not support ranges in switch-case statements. Use GCC or Clang"
+#endif
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
