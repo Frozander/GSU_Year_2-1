dfff9f2a2ff7cb071114141c747f3d7cf36d2c00
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2019-12-17 21:55:05 +0300

    [TP09] Added comments for functions

--- TPs/09/students.h
+++ TPs/09/students.h
@@ -48,10 +48,17 @@ typedef struct __student_bst
     struct __student_bst *right;
 } Student_BST;
 
-States add_student(Student_DL **root, char *name, char *surname);
+// Combines all other auxhilary functions for grading,
+// encryption and calculation to create a student
+// with random grades.
 Student *create_student(char *name, char *surname);
 
+// Inserts a stundent with random grades into a Double Linked List
+States add_student(Student_DL **root, char *name, char *surname);
+
+// Auxhilary function to give a random grade between [0 - 100] to a student
 float give_random_note();
+// Auxhilary function to give a random credit weight for a class
 int give_random_credits();
 
 // Takes a numerical grade in the interval [0, 100]
@@ -65,12 +72,22 @@ int give_random_credits();
 // For unexpected values (e.g. -3, 132) the function returns: "NaN"
 char *num_to_char_grade(float num_grade);
 
+// Auxhilary function that encrypts a floating point value (intended to use for means)
+// using 2 char values (preferably first letters of a students name and surname)
 float encrypt_student_mean(float mean, char key1, char key2);
+// Auxhilary function that decrypts a floating point value (intended to use for means)
+// using 2 char values (preferably first letters of a students name and surname)
 float decrypt_student_mean(float mean, char key1, char key2);
 
+// Inserts a stundent with random grades into a Binary Search Tree
 States add_tree_node(Student_BST **root, Student *student_data);
+// Feeds student data from a Double Linked List to a Binary Search Tree
 Student_BST *feed_to_tree(Student_DL *DL_list);
 
+// Prints all the data from a Student struct
+void print_student(Student *student_data);
+// Prints out the students with higher mean than the threshold
+// The function decrypts the value inside it.
 void means_with_threshold(Student_DL *DL_list, float threshold);
 
 #endif
